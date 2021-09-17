4b4aecd2df0007bf89d17450721650bf24c5577a
DEV <dev@info.local>
2021-09-17 13:20:45 -0300

    verificação de chassi valido

--- c05 - CHARP_POO/Program.cs
+++ c05 - CHARP_POO/Program.cs
@@ -55,8 +55,16 @@ namespace c05___CHARP_POO
                         Console.WriteLine($"Digite o chassi do veiculo");
                         string chassiPesquisado = Console.ReadLine();
                         Veiculo veiculoPesquisado = VeiculoController.BuscarVeiculo(chassiPesquisado, listaVeiculo);
-                        Console.WriteLine($"Veiculo retornado {veiculoPesquisado.Modelo}");
-                        
+                        if (veiculoPesquisado == null)
+                        {
+                            Console.WriteLine($"Veiculo não encontrado");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Veiculo retornado {veiculoPesquisado.Modelo}");
+                        }
+
                         break;
 
                     default:
