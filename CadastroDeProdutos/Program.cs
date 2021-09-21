65815834e7114f8260029c03bc97d0ca1dadec53
DEV <dev@info.local>
2021-09-21 16:59:15 -0300

    Criação de um CRUD cadastro de produtos

---
+++ CadastroDeProdutos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using CadastroDeProdutos.Controller;
+using CadastroDeProdutos.Models;
+
+namespace cadastrodeprodutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncionariosController funcionario = new FuncionariosController();
+            List<Funcionario> listaFuncionarios = new List<Funcionario>();
+
+            funcionario.CadastroDeFuncionarios(listaFuncionarios);
+            funcionario.LoginFuncionario(listaFuncionarios);
+
+
+        }
+    }
+}
